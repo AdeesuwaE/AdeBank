e23b31b23269ba45d0a25b3519deca5180eab3ad
AdeesuwaE <159738681+AdeesuwaE@users.noreply.github.com>
2024-10-23 14:15:38 -0400

    Add files via upload

---
+++ src/main/java/learn_spring_framework/ade/AdeApplication.java
@@ -0,0 +1,13 @@
+package learn_spring_framework.ade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/learn_spring_framework/ade/AdeApplicationTests.java
@@ -0,0 +1,13 @@
+package learn_spring_framework.ade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
