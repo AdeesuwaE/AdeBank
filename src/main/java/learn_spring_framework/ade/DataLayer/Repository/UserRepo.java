aa352c346e50ff37e837afa1eee3a59dfabbe4dc
adesuwaE <adesuwa411@gmail.com>
2024-10-31 09:51:06 -0400

    "Added comment"

--- src/main/java/learn_spring_framework/ade/DataLayer/Repository/UserRepo.java
+++ src/main/java/learn_spring_framework/ade/DataLayer/Repository/UserRepo.java
@@ -4,7 +4,8 @@ import learn_spring_framework.ade.DataLayer.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Repository // Can use @Component but @Repository is more specific
+//Does communication with the DB
 public interface UserRepo extends JpaRepository<User, Long> {
     User getUserByEmail(String email);
 
